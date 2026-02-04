d197c9adb21c51e5d88b820d0bc9144a12e2229a
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-02-04 21:37:18 +0530

    [GurleenKaur]: add spring configuration and component scan

---
+++ src/main/java/com/gevernova/configuration/Config.java
@@ -0,0 +1,9 @@
+package com.gevernova.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.gevernova")
+public class Config {
+}
